de9008edb21b4c9bdbcb55673805936f5b472daa
GiaBaone <102190252@sv1.dut.udn.vn>
2022-03-11 14:58:11 +0700

    migrate_role 📝

--- api_account/migrations/0003_migrate_role.py
+++ api_account/migrations/0003_migrate_role.py
@@ -2,12 +2,29 @@
 
 from django.db import migrations
 
+from api_account.contants import RoleData
+
 
 class Migration(migrations.Migration):
+
+    def initial_role_data(apps, schema_editor):
+        role_model = apps.get_model("api_account", "Role")
+
+        roles = []
+
+        for role in RoleData:
+            roles.append(role_model(id=role.value['id'], name=role.value['name']))
+
+        role_model.objects.bulk_create(roles)
+
+    def delete_all_data(apps, schema_editor):
+        role_model = apps.get_model("api_account", "Role")
+        role_model.objects.all().delete()
 
     dependencies = [
         ('api_account', '0002_alter_account_role'),
     ]
 
     operations = [
+        migrations.RunPython(initial_role_data, delete_all_data())
     ]
