db7c11cd550a4f77865d11d8366424da8294bbe9
GiaBaone <102190252@sv1.dut.udn.vn>
2022-03-23 14:23:25 +0700

    update model for Product 🛠

--- api_product/models/Product.py
+++ api_product/models/Product.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 from api_base.models import TimeStampedModel
-from api_product.models import ProductBatch
+from api_product.models import ProductBatch, Category
 
 
 class Product(TimeStampedModel):
     name = models.CharField(max_length=50)
     status = models.BooleanField(null=True, blank=True)
 
-    product_batch = models.ForeignKey(ProductBatch, on_delete=models.CASCADE, related_name="products")
+    product_batch = models.ForeignKey(ProductBatch, on_delete=models.CASCADE, related_name="products", null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", null=True, blank=True)
 
     class Meta:
         db_table = 'product'
+        ordering = ('created_at',)
--- api_product/models/__init__.py
+++ api_product/models/__init__.py
@@ -1,3 +1,4 @@
+from .Category import Category
 from .ProductBatch import ProductBatch
 from .Product import Product
 from .ProductImage import ProductImage
