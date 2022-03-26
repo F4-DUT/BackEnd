ce6088d1704aad0023f8b6962d4fd47086274ea1
GiaBaone <102190252@sv1.dut.udn.vn>
2022-03-27 00:24:21 +0700

    edit get products by category 🛠

--- api_product/views/Category.py
+++ api_product/views/Category.py
@@ -1,12 +1,26 @@
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from api_base.views import BaseViewSet
-from api_product.models import Category
-from api_product.serializers import CategorySerializer
+from api_product.models import Category, Product
+from api_product.serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryViewSet(BaseViewSet):
     queryset = Category.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def get_products(self, request, pk):
+        category = self.get_object()
+
+        products = Product.objects.filter(category=category)
+        if products.exists():
+            rs = ProductSerializer(products, many=True)
+            return Response(rs.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error_message": "product is not defined! "}, status=status.HTTP_400_BAD_REQUEST)
 
--- api_product/views/Product.py
+++ api_product/views/Product.py
@@ -3,9 +3,12 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from api_account.permission import RaspberryPermission
 from api_base.views import BaseViewSet
 from api_product.models import Product, Category
 from api_product.serializers import ProductSerializer
+
+
 
 
 class ProductViewSet(BaseViewSet):
@@ -14,16 +17,11 @@ class ProductViewSet(BaseViewSet):
     permission_classes = [IsAuthenticated]
 
     permission_map = {
-
+        "send_image": [RaspberryPermission]
     }
 
-    @action(detail=False, methods=['get'])
-    def get_list_by_category(self, request):
-        category = Category.objects.filter(id=request.data.get('category_id')).first()
 
-        products = Product.objects.filter(category=category)
-        if products.exists():
-            rs = ProductSerializer(products, many=True)
-            return Response(rs.data, status=status.HTTP_200_OK)
-        else:
-            return Response({"error_message": "product is not defined! "}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
