d7562ed15dbf049b3b9e9fa66d8d082d166a29ef
giabao2807 <dinhgiabao2807@gmail.com>
2022-03-31 21:03:43 +0700

    [FIX] change ACCESS_TOKEN_LIFETIME

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
