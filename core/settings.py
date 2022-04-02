eb71afb06c77eb87d0bcde7d0bc270b95b89557f
GiaBaone <102190252@sv1.dut.udn.vn>
2022-04-03 00:09:17 +0700

    edit TIME_ACCESS_TOKEN to 5 minutes 🛠

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
