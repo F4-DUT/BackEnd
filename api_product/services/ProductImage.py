c41a8560d4a1afaad2aeee56bb5edddc7a2c206a
GiaBaone <102190252@sv1.dut.udn.vn>
2022-04-05 08:04:01 +0700

    API for getProduct by image_id 🔖

--- api_product/services/ProductImage.py
+++ api_product/services/ProductImage.py
@@ -16,7 +16,21 @@ class ProductImageService:
         return upload_image.get('url')
 
     @classmethod
+    def upload_images(cls, images):
+        images_url = []
+        for image in images:
+            upload_image = cloudinary.uploader.upload(image, folder=os.getenv('CLOUDINARY_PRODUCT_FOLDER'))
+            images_url.append(upload_image.get('url'))
+        return images_url
+
+    @classmethod
     def create(cls, image_url, product):
         product_image = ProductImage(image=image_url, product=product)
         product_image.save()
         return product_image
+
+    @classmethod
+    def create(cls, image_urls, product):
+        for image_url in image_urls:
+            product_image = ProductImage(image=image_url, product=product)
+            product_image.save()
--- api_product/views/ProductImage.py
+++ api_product/views/ProductImage.py
@@ -1,9 +1,11 @@
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 
 from api_base.views import BaseViewSet
 from api_product.models import ProductImage
-from api_product.serializers import ProductImageSerializer
+from api_product.serializers import ProductImageSerializer, ProductSerializer
 
 
 class ProductImageViewSet(BaseViewSet):
@@ -13,3 +15,11 @@ class ProductImageViewSet(BaseViewSet):
 
     permission_map = {
     }
+
+    @action(detail=True, methods=['get'])
+    def get_product_by_image(self, request, pk):
+        product = self.get_object().product
+        serializer = ProductSerializer(product)
+        if serializer:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"error_message": "Product is not defined!!"}, status=status.HTTP_400_BAD_REQUEST)
